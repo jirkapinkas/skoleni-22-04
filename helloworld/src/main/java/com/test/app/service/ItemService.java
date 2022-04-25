327dcfe7195a0e684768309900b1fb9a113ba68d
Jirka Pinkas <jirka.pinkas@gmail.com>
2022-04-25 14:15:14 +0200

    + @PostConstruct

--- helloworld/src/main/java/com/test/app/service/ItemService.java
+++ helloworld/src/main/java/com/test/app/service/ItemService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 @RequiredArgsConstructor
 public class ItemService {
@@ -15,6 +17,11 @@ public class ItemService {
 //    public ItemService(@Lazy ItemRepository itemRepository) {
 //        this.itemRepository = itemRepository;
 //    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("ItemService constructed!");
+    }
 
     public String getCount() {
         return "items count = " + itemRepository.count();
