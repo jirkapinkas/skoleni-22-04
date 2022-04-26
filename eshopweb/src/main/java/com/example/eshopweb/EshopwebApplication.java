723c3fc08a6523ce36d188cfecc5f89ff01c20de
Jirka Pinkas <jirka.pinkas@gmail.com>
2022-04-26 10:17:37 +0200

    hello world spring boot II.

--- eshopweb/src/main/java/com/example/eshopweb/EshopwebApplication.java
+++ eshopweb/src/main/java/com/example/eshopweb/EshopwebApplication.java
@@ -1,8 +1,14 @@
 package com.example.eshopweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
 @SpringBootApplication
 public class EshopwebApplication {
 
