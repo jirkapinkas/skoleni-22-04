99dcbc0d066549ce40522b3231a25a6c376c51fa
Jirka Pinkas <jirka.pinkas@gmail.com>
2022-04-27 14:59:24 +0200

    + spring security!!! USERNAME: jirka, PASSWORD: jirka

---
+++ eshopweb/src/main/java/com/example/eshopweb/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.eshopweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**")
+                .hasRole("ADMIN")
+                .antMatchers("/**")
+                .hasRole("USER")
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                .logout();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.builder()
+                        .username("jirka")
+                        .password("{noop}jirka")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
